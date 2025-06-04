581171439b5f2be0b78c5be9b09133deae29360d
ihibti <ismaelhibti2000@gmail.com>
2025-06-04 08:53:07 -0400

    ftp config

---
+++ srcs/requirements/bonus/ftp/entrypoint.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -e
+
+echo "🚀 Starting vsftpd..."
+exec /usr/sbin/vsftpd /etc/vsftpd.conf
+#!/bin/bash
+set -e
+
+echo "🚀 Starting vsftpd..."
+exec /usr/sbin/vsftpd /etc/vsftpd.conf
